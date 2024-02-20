606f3a21680a54c5814e29950d194aa42bfcb7e3
mufaqar <mufaqar@gmail.com>
2024-02-20 16:24:31 +0500

    Zara Favicon

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,16 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      boxShadow: {
+        '3xl': '0 2px 11px 0px rgba(160, 152, 211, 0.2)',
+      },
+      colors: {
+        primary: '#7070FF',
+        icon: '#B6B9CE',
+        title: '#5E5E6F',
+        body_text: '#7F7F7F',
+        line: '#EAEAF1',
+      },
     },
   },
   plugins: [],
